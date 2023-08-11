ace7be9745af24e39d736ae5f63e20cea9a83f9a
J. Silva <112393484+jsilva-tab@users.noreply.github.com>
2023-08-11 14:35:36 -0300

    Add files via upload

---
+++ 11_08.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    float a,b;
+    puts("Digite um número:");
+    scanf("%f",&a);
+    puts("Digite um número:");
+    scanf("%f",&b);
+
+    printf("Resultado:%.2f \n",a*b);
+}
+
