66ffd35cd4415b6e91ed0809f9916645155e47f7
J. Silva <112393484+jsilva-tab@users.noreply.github.com>
2023-08-18 15:34:38 -0300

    Add files via upload

--- aulahoje.c
+++ aulahoje.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//resto da divisão
-
 int main(){
-    float n1, restodiv;
-    scanf("%f",&n1);
-    restodiv = ((int)n1%3) + (n1-(int)n1);
-    printf("%f \n",restodiv);
+    int num_f,h_f,num_filho;
+    float val_hora,sal,sal_final;
+    puts("Digite o seu número de identificação:");
+    scanf("%d",&num_f);
+    puts("Digite suas horas trabalhadas:");
+    scanf("%d",&h_f);
+    puts("Digite o valor ganho por hora:");
+    scanf("%f",&val_hora);
+    puts("Informe quantos filhos você tem com idade menor de 14 anos:");
+    scanf("%d",&num_filho);
+
+    sal = h_f*val_hora;
+
+    if(num_filho > 0){
+        sal_final = sal + (sal / (num_filho * 0.1));
+
+        puts("************");
+
+        printf("ID:%d\nHoras Trabalhadas:%d\nValor por Hora:R$%.2f\nNum. filhos:%d\nSalário Final:R$%.2f\n",num_f,h_f,val_hora,num_filho,sal_final);
+    } else{
+        puts("************");
+        printf("ID:%d\nHoras Trabalhadas:%d\nValor por Hora:R$%.2f\nNum. filhos:%d\nSalário Final:R$%.2f\n",num_f,h_f,val_hora,num_filho,sal);
+    }
 }
